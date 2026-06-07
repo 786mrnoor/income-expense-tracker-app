86f6d088b1d91b753e897929178ed3a24135e77e
Noor Mohammad <786mrnoor@gmail.com>
2026-06-07 21:41:04 +0530

    fix: unhandled response header

--- frontend/src/utils/custom-fetch.ts
+++ frontend/src/utils/custom-fetch.ts
@@ -1,15 +1,18 @@
 import z from "zod";
-import type { FetchConfig } from "./custom-fetch.types";
 import { buildQuery } from "./build-query";
+import type { FetchConfig } from "./custom-fetch.types";
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
-export async function customFetch<T extends z.ZodTypeAny>(config: FetchConfig<T>): Promise<z.infer<T>>;
+export async function customFetch<T extends z.ZodTypeAny>(
+  config: FetchConfig<T>,
+): Promise<z.infer<T>>;
 export async function customFetch(config: FetchConfig): Promise<any>;
 
-export async function customFetch<T extends z.ZodTypeAny | undefined>(config: FetchConfig<T>) {
-
-  let url = BACKEND_URL + config.path + buildQuery(config.query);
+export async function customFetch<T extends z.ZodTypeAny | undefined>(
+  config: FetchConfig<T>,
+) {
+  const url = BACKEND_URL + config.path + buildQuery(config.query);
 
   const res = await fetch(url, {
     ...config,
@@ -17,11 +20,11 @@ export async function customFetch<T extends z.ZodTypeAny | undefined>(config: Fe
     headers: {
       ...config.headers,
       ...(config.body ? { "Content-Type": "application/json" } : {}),
-    }
+    },
   });
 
   let json;
-  if (!res.headers.get("content-length")) {
+  if (!res.headers.get("content-type")?.includes("application/json")) {
     json = {};
   } else {
     json = await res.json();
@@ -32,7 +35,7 @@ export async function customFetch<T extends z.ZodTypeAny | undefined>(config: Fe
       ...json,
       success: false,
       statusCode: res.status,
-      message: json.message || res.statusText
+      message: json.message || res.statusText,
     };
   }
 
