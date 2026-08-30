d1b85fdcacfa0c927fb8d26e7bb4a364537c20c7
Noor Mohammad <786mrnoor@gmail.com>
2026-08-30 15:59:34 +0530

    add lazy loading on accounts and routes pages

--- frontend/src/app/(auth)/routes.tsx
+++ frontend/src/app/(auth)/routes.tsx
@@ -1,22 +1,28 @@
+import { lazy } from "react";
+
 import { ErrorScreen } from "@/components/error-screen";
 import { Route } from "react-router";
 import AuthLayout from "./layout";
 import Index from "./index/page";
 import AddTransactionPage from "./add-transaction/page";
 import EditTransactionPage from "./edit-transaction/page";
-import TagPage from "./tags/page";
-import AccountPage from "./accounts/page";
 
-export const AuthRoutes =
-  <Route path="/"
+const TagPage = lazy(() => import("./tags/page"));
+const AccountPage = lazy(() => import("./accounts/page"));
+
+export const AuthRoutes = (
+  <Route
+    path="/"
     element={
       <ErrorScreen>
         <AuthLayout />
       </ErrorScreen>
-    }>
+    }
+  >
     <Route index element={<Index />} />
     <Route path="add-transaction" element={<AddTransactionPage />} />
     <Route path="edit-transaction/:id" element={<EditTransactionPage />} />
     <Route path="tags" element={<TagPage />} />
     <Route path="accounts" element={<AccountPage />} />
   </Route>
+);
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   resolve: {
-    tsconfigPaths: true
+    tsconfigPaths: true,
   },
   server: {
     proxy: {
-      '/api': 'http://localhost:3000'
-    }
+      "/api": "http://localhost:3000",
+    },
   },
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-})
+  plugins: [react(), tailwindcss()],
+});
